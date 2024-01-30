30d382c622049777b336ceeadfc946cf7646446a
lukman-hakim-123 <lh146244@gmail.com>
2024-01-30 11:29:44 +0700

    polimorphism

---
+++ oop/lib/parameter_inherit.dart
@@ -0,0 +1,19 @@
+class Binatang {
+  String jenis;
+
+  Binatang(this.jenis);
+
+  void bersuara() {
+    print("Sebuah binatang dari jenis $jenis bersuara");
+  }
+}
+
+class Kucing extends Binatang {
+  String jenisBulu;
+
+  Kucing(String jenis, this.jenisBulu) : super(jenis);
+
+  void tidur() {
+    print('Kucing tidur dengan nyenyak.');
+  }
+}
