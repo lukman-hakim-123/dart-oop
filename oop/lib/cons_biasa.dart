9ef3385f9b8d53c9baee76c5d48fccf3e4021b1d
lukman-hakim-123 <lh146244@gmail.com>
2024-01-30 09:37:39 +0700

    construktor

---
+++ oop/lib/cons_biasa.dart
@@ -0,0 +1,21 @@
+class Student {
+  String? name;
+  int? age;
+  int? rollNumber;
+
+  // Student() {
+  //   print("default construktor");
+  // }
+  Student.namedConstructor(String name, int age, int rollNumber) {
+    this.name = name;
+    this.age = age;
+    this.rollNumber = rollNumber;
+  }
+}
+
+void main() {
+  Student student = Student.namedConstructor("John", 20, 1);
+  print("Name: ${student.name}");
+  print("Age: ${student.age}");
+  print("Roll Number: ${student.rollNumber}");
+}
